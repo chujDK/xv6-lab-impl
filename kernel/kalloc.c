b539b26bf7e181b95e36464f2ef89b6e4ab9269e
chujDK <1768485949@qq.com>
2022-09-02 13:43:44 +0800

    add lab lock: memory allocator, test pass

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,13 +21,43 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem, kmem_percpu[NCPU];
+
+struct spinlock shrink_lock;
 
 void
 kinit()
 {
+  char lock_name[16];
+
   initlock(&kmem.lock, "kmem");
+
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(lock_name, sizeof(lock_name), "kmem_%d", i);
+    initlock(&kmem_percpu[i].lock, lock_name);
+  }
+
+  initlock(&shrink_lock, "kmem_shrink");
+
   freerange(end, (void*)PHYSTOP);
+}
+
+void
+kfree_internal(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree_internal");
+
+  r = (struct run*)pa;
+
+  memset(pa, 1, PGSIZE);
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
 }
 
 void
@@ -36,7 +66,34 @@ freerange(void *pa_start, void *pa_end)
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    kfree_internal(p);
+}
+
+void
+shrink_cpu_freelist(int cpuid)
+{
+  struct run* tmp;
+
+  acquire(&kmem_percpu[cpuid].lock);
+  for (struct run* p = kmem_percpu[cpuid].freelist; p != 0;) {
+    tmp = p;
+    p = p->next;
+    kfree_internal(tmp);
+  }
+  kmem_percpu[cpuid].freelist = 0;
+  release(&kmem_percpu[cpuid].lock);
+}
+
+void
+shrink_free(int caller_cpuid)
+{
+  acquire(&shrink_lock);
+  for (int i = 0; i < NCPU; i++) {
+    if (i != caller_cpuid) {
+      shrink_cpu_freelist(i);
+    }
+  }
+  release(&shrink_lock);
 }
 
 // Free the page of physical memory pointed at by v,
@@ -47,26 +104,28 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int mycpu;
+  push_off();
+  mycpu = cpuid();
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
+  // Fill with junk to catch dangling refs
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem_percpu[mycpu].lock);
+  r->next = kmem_percpu[mycpu].freelist;
+  kmem_percpu[mycpu].freelist = r;
+  release(&kmem_percpu[mycpu].lock);
+
+  pop_off();
 }
 
-// Allocate one 4096-byte page of physical memory.
-// Returns a pointer that the kernel can use.
-// Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
+kalloc_internal(void)
 {
   struct run *r;
 
@@ -76,7 +135,39 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
+  return (void*)r;
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+  int mycpu;
+
+  push_off();
+  mycpu = cpuid();
+
+  acquire(&kmem_percpu[mycpu].lock);
+  r = kmem_percpu[mycpu].freelist;
+  if (r) {
+    kmem_percpu[mycpu].freelist = r->next;
+  } else {
+    // try to alloc from internal freelist
+    r = kalloc_internal();
+    if (!r) {
+      // failed, try to steal from other cpus
+      shrink_free(mycpu);
+      r = kalloc_internal();
+    }
+  }
+
+  release(&kmem_percpu[mycpu].lock);
+
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  pop_off();
   return (void*)r;
 }
